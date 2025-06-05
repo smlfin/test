e97ee3c61a660955e46a668018c0ca6a03b7031a
smlfin <senioravp71@gmail.com>
2025-06-05 20:53:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // *** Configuration ***
-    const DATA_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTO7LujC4VSa2wGkJ2YEYSN7UeXR221ny3THrVegYfNfRm2JQGg7QR9Bxxh9SadXtK8Pi6-psl2tGsb/pub?gid=696550092&single=true&output=csv";
+    const DATA_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTO7LujC4VSa2wGkJ2YEYSN7UeXR221ny3THaVegYfNfRm2JQGg7QR9Bxxh9SadXtK8Pi6-psl2tGsb/pub?gid=696550092&single=true&output=csv";
     const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyr_LP1bpaoszUtWAlMeFxFIwzsUQF0USmdTRaTdqBHcn9HFo82nmBqQt4zokKCuHL5/exec"; // <--- Updated URL!
 
     const MONTHLY_WORKING_DAYS = 22; // Common approximation for a month's working days
